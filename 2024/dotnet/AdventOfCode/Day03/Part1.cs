20540652601226742c3ae65480fc413eb04f6505
byCrookie <github@bycrookie.com>
2024-12-03 17:40:28 +0100

    day 3 optimize

--- 2024/dotnet/AdventOfCode/Day03/Part1.cs
+++ 2024/dotnet/AdventOfCode/Day03/Part1.cs
@@ -10,15 +10,9 @@ public partial class Part1 : IPart
 {
     public Task<PartResult> RunAsync(IMeasure measure, string input)
     {
-        var regex = MulRegex();
-        var sum = 0;
-
-        foreach (Match match in regex.Matches(input))
-        {
-            var multiplicand = int.Parse(match.Groups["multiplicand"].Value);
-            var multiplier = int.Parse(match.Groups["multiplier"].Value);
-            sum += multiplicand * multiplier;
-        }
+        var sum = MulRegex().Matches(input)
+            .Aggregate(0, (acc, match) => acc + int.Parse(match.Groups["multiplicand"].Value)
+                * int.Parse(match.Groups["multiplier"].Value));
 
         return Task.FromResult(new PartResult($"{sum}", $"Sum of all multiplications is {sum}"));
     }
--- 2024/dotnet/AdventOfCode/Day03/Part2.cs
+++ 2024/dotnet/AdventOfCode/Day03/Part2.cs
@@ -10,33 +10,17 @@ public partial class Part2 : IPart
 {
     public Task<PartResult> RunAsync(IMeasure measure, string input)
     {
-        var regex = MulRegex();
-        var sum = 0;
         var dont = false;
-        
-        foreach (Match match in regex.Matches(input))
-        {
-            if (match.Groups["dont"].Success)
+        var sum = MulRegex().Matches(input)
+            .Aggregate(0, (acc, match) =>
             {
-                dont = true;
-                continue;
-            }
-            
-            if (match.Groups["do"].Success)
-            {
-                dont = false;
-                continue;
-            }
-            
-            if (dont)
-            {
-                continue;
-            }
-            
-            var multiplicand = int.Parse(match.Groups["multiplicand"].Value);
-            var multiplier = int.Parse(match.Groups["multiplier"].Value);
-            sum += multiplicand * multiplier;
-        }
+                if (match.Groups["dont"].Success) dont = true;
+                else if (match.Groups["do"].Success) dont = false;
+                else if (!dont)
+                    return acc + int.Parse(match.Groups["multiplicand"].Value) *
+                        int.Parse(match.Groups["multiplier"].Value);
+                return acc;
+            });
 
         return Task.FromResult(new PartResult($"{sum}", $"Sum of all multiplications is {sum}"));
     }
