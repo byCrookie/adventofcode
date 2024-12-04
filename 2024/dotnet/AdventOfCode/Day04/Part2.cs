adcafe22a92ae5411e2a6096acd725fe20149bbb
byCrookie <github@bycrookie.com>
2024-12-04 20:04:00 +0100

    day 4 part2

--- 2024/dotnet/AdventOfCode/Day04/Part2.cs
+++ 2024/dotnet/AdventOfCode/Day04/Part2.cs
@@ -9,6 +9,60 @@ public class Part2 : IPart
 {
     public Task<PartResult> RunAsync(IMeasure measure, string input)
     {
-        return Task.FromResult(new PartResult($"", $"Sum of all multiplications is"));
+        var lines = input.Split(Environment.NewLine);
+        var field = new char[lines.Length][];
+        for (var i = 0; i < lines.Length; i++)
+        {
+            field[i] = lines[i].ToCharArray();
+        }
+
+        measure.Now("Parsed");
+
+        var coords = new List<Coord>();
+        for (var row = 0; row < field.Length; row++)
+        {
+            for (var col = 0; col < field[row].Length; col++)
+            {
+                coords.Add(new Coord(col, row, field[row].Length, field.Length));
+            }
+        }
+
+        var sum = coords.Sum(coord => Search(field, coord));
+        return Task.FromResult(new PartResult($"{sum}", $"Sum of all multiplications is {sum}"));
     }
+
+    private static int Search(char[][] field, Coord coord)
+    {
+        if (field[coord.Y][coord.X] != 'A')
+        {
+            return 0;
+        }
+
+        for (var row = -1; row <= 1; row++)
+        {
+            for (var col = -1; col <= 1; col++)
+            {
+                if (coord.X + row < 0 || coord.X + row >= coord.MaxX || coord.Y + col < 0 ||
+                    coord.Y + col >= coord.MaxY)
+                {
+                    return 0;
+                }
+            }
+        }
+
+        var topLeft = field[coord.Y - 1][coord.X - 1];
+        var topRight = field[coord.Y - 1][coord.X + 1];
+        var bottomLeft = field[coord.Y + 1][coord.X - 1];
+        var bottomRight = field[coord.Y + 1][coord.X + 1];
+
+        if (((topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M'))
+            && ((topRight == 'M' && bottomLeft == 'S') || (topRight == 'S' && bottomLeft == 'M')))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private record Coord(int X, int Y, int MaxX, int MaxY);
 }
