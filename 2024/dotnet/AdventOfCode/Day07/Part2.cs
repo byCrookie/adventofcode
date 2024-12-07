3242fb32f7b3309ae16a58496a69b65ea4cc6c90
bycrookie <github@bycrookie.com>
2024-12-07 09:22:27 +0100

    day 7 part 2

--- 2024/dotnet/AdventOfCode/Day07/Part2.cs
+++ 2024/dotnet/AdventOfCode/Day07/Part2.cs
@@ -7,8 +7,57 @@ namespace AdventOfCode.Day07;
 [Part(2)]
 public class Part2 : IPart
 {
+    private static readonly List<Func<long, long, long>> Operations =
+    [
+        (a, b) => a + b,
+        (a, b) => a * b,
+        (a, b) => long.Parse(string.Concat(a, b))
+    ];
+
     public async Task<PartResult> RunAsync(IMeasure measure, string input)
     {
-        return new PartResult($"", $"Count of additional obstacles that result in loop");
+        var validTests = new List<long>();
+        foreach (var line in input.Split(Environment.NewLine))
+        {
+            var parts = line.Split(":");
+            var comp = long.Parse(parts[0]);
+            var numbers = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            var valid = false;
+            foreach (var op in Operations)
+            {
+                valid |= Compare(comp, numbers, 1, numbers[0], op);
+            }
+
+            if (valid)
+            {
+                validTests.Add(comp);
+            }
+        }
+
+        var sum = validTests.Sum();
+        return new PartResult($"{sum}", $"Count of additional obstacles that result in loop {sum}");
+    }
+
+    private static bool Compare(long comp, long[] numbers, long index, long result, Func<long, long, long> operation)
+    {
+        if (index == numbers.Length)
+        {
+            if (result == comp)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        var current = numbers[index];
+        result = operation(result, current);
+        var valid = false;
+        foreach (var op in Operations)
+        {
+            valid |= Compare(comp, numbers, index + 1, result, op);
+        }
+
+        return valid;
     }
 }
