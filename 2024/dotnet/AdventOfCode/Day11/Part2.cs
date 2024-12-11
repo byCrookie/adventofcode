981bd5100c02f1105acf30f6b2ed35b5fc935332
byCrookie <github@bycrookie.com>
2024-12-11 21:11:58 +0100

    day 11 part 2 optimize

--- 2024/dotnet/AdventOfCode/Day11/Part2.cs
+++ 2024/dotnet/AdventOfCode/Day11/Part2.cs
@@ -10,41 +10,37 @@ public class Part2 : IPart
     public Task<PartResult> RunAsync(IMeasure measure, string input)
     {
         const long blinks = 75;
-        var stones = input.Split(' ').Select(long.Parse).ToList();
+        var stones = input.Split(' ').Select(long.Parse);
         // Console.WriteLine($"Blink 0: {string.Join(", ", stones.Select(s => s.Num))}");
 
         for (var blink = 0; blink < blinks; blink++)
         {
-            var newStones = new List<long>();
-
-            for (var i = 0; i < stones.Count; i++)
+            stones = stones.SelectMany(stone =>
             {
-                var stone = stones[i];
-
                 if (stone == 0)
                 {
-                    newStones.Add(1);
-                    continue;
+                    return [1L];
                 }
 
-                var stoneString = stone.ToString();
-                if (stoneString.Length % 2 == 0)
+                var length = stone.Equals(0) ? 1L : (long)Math.Log10(stone) + 1L;
+                if (length % 2 == 0L)
                 {
-                    var left = long.Parse(stoneString[..(stoneString.Length / 2)]);
-                    var right = long.Parse(stoneString[(stoneString.Length / 2)..]);
-                    newStones.Add(left);
-                    newStones.Add(right);
-                    continue;
+                    // ReSharper disable once PossibleLossOfFraction
+                    var x = (long)Math.Pow(10L, length / 2);
+                    var left = stone / x;
+                    var right = stone % x;
+                    return [left, right];
                 }
 
-                newStones.Add(stone * 2024);
-            }
+                return new[] { stone * 2024 };
+            });
 
-            stones = newStones;
             // Console.WriteLine($"Blink {blink + 1}: {string.Join(", ", stones.Select(s => s.Num))}");
-            Console.WriteLine($"Blink {blink + 1}: {stones.Count}");
+            // Console.WriteLine($"Blink {blink + 1}: {stones.Count()}");
+            // Console.WriteLine($"Blink {blink + 1}");
         }
 
-        return Task.FromResult(new PartResult($"{stones.Count}", $"Amount of stones after blinking: {stones.Count}"));
+        var count = stones.Count();
+        return Task.FromResult(new PartResult($"{count}", $"Amount of stones after blinking: {count}"));
     }
 }
